34b203d3e33c55e1affdc618e38338f29d43e9b6
Bryan Pitman <pitman.bryan@gmail.com>
2022-08-22 11:53:17 -0700

    first 3 models complete

--- models/user.js
+++ models/user.js
@@ -4,27 +4,77 @@
 
 class User {
 
+  constructor({ username, password, first_name, last_name, phone }) {
+    this.username = username;
+    this.password = password;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.phone = phone;
+  }
+
   /** Register new user. Returns
    *    {username, password, first_name, last_name, phone}
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    if (this.username === undefined) {
+      const result = await db.query(
+        `INSERT INTO customers
+            (username, password, first_name, last_name, phone, notes)
+             VALUES ($1, $2, $3, $4, $5, $6)
+             RETURNING username, password, first_name, last_name, phone, notes`,
+        [this.username,
+        this.password,
+        this.firstName,
+        this.lastName,
+        this.phone,
+        this.notes],
+      );
+      return result.rows[0];
+
+    }
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+         FROM users
+         WHERE username = $1`,
+      [username]);
+    const user = result.rows[0];
+    return user;
+
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE messages
+       SET last_login_at = current_timestamp
+         WHERE username = $1
+         RETURNING username, last_login_at`,
+      [username]);
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No such user: ${username}`);
+
+    return user;
+
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+     const results = await db.query(
+     `SELECT username, first_name, last_name
+      FROM users`,
+     )
+    return results;
+
   }
 
   /** Get: get user by username
